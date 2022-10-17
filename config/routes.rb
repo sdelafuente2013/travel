30b5b2aa764ab63ead66a593307773b21d451e56
delafuente <delafuente@tirant.com>
2022-10-17 15:46:09 +0200

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'users#index'
 end
