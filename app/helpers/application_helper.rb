21ac0fac4349c080c54a69399810276518036834
delafuente <delafuente@tirant.com>
2022-10-21 16:18:07 +0200

    style: fix cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def generate_img(url_img, ciudad, pais)
+    "<div class='card'>
+      <img src=#{url_img}>
+      <p class='name'>#{ciudad}</p>
+      <p class='pais'>#{pais}</p>
+    </div>"
+  end
 end
