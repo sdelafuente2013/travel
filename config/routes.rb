cae51822568a09003622e9aa5d9422b705afc949
delafuente <delafuente@tirant.com>
2022-10-28 12:57:27 +0200

    style: fix background white

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'guides/info'
   get 'guides/form'
   get '/', to: 'users#index', as: 'user_root'
-  post '/guides/form', to: 'guides#save'
+  post 'guides/form', to: 'guides#save'
 end
