52f06f8131a57b0497c6128c7ebc6b95e3008da7
delafuente <delafuente@tirant.com>
2022-10-31 21:07:50 +0100

    fix: fix schemma db

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,23 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_081954) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_201503) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'contacts', force: :cascade do |t|
-    t.string 'name'
-    t.string 'phone'
-    t.string 'email'
-    t.string 'about'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_confirm"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'password'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
 end
