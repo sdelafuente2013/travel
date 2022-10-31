b14fbd651d86366b5803d8d78a355d9b1b8c65d1
delafuente <delafuente@tirant.com>
2022-10-31 21:24:09 +0100

    fix: fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :members
+  # devise_for :members
 
   get 'form/create', to: 'forms#create_form'
   post 'form/create', to: 'forms#send_mails'
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
   get 'owners/dashboard', to: 'owners#dashboard'
   get 'users/dashboard', to: 'users#dashboard'
 
-  # devise_for :members, path: '', path_names: {
-  #   sign_in: 'login',
-  #   sign_out: 'logout',
-  #   password: 'secret',
-  #   confirmation: 'verification',
-  #   unlock: 'unblock',
-  #   registration: 'register',
-  #   sign_up: 'signup' }
+  devise_for :members, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'signup' }
 end
