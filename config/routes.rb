b3a4f7eebec07e9c161338bc3575df79267bfc2a
delafuente <delafuente@tirant.com>
2022-10-31 21:15:22 +0100

    add: add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :members
 
   get 'form/create', to: 'forms#create_form'
   post 'form/create', to: 'forms#send_mails'
