b9b6773f6ed858a060df67b5a59a880244564810
delafuente <delafuente@tirant.com>
2022-10-31 22:13:23 +0100

    fix: fix rubocop styles

--- db/migrate/20221030201607_remove_about_to_contacts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveAboutToContacts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :contacts, :about, :string
-    remove_column :contacts, :phone, :string
-  end
-end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe 'HTTP response status codes' do
     context 'when accessing "/"' do
-      before { get :dashboard }
+      before { get :index }
 
       it 'should give me a code 200' do
         should respond_with(200)
@@ -15,7 +15,7 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'HTTP Methods' do
     context 'when #index is accessed' do
-      before { get :dashboard }
+      before { get :index }
 
       it 'should render the index.html view' do
         should render_template('index')
@@ -26,7 +26,7 @@ RSpec.describe UsersController, type: :controller do
   describe 'Routing', type: :routing do
     context 'when accessing "/"' do
       it 'should action #index' do
-        should route(:get, '/').to(controller: :users, action: :dashboard)
+        should route(:get, '/').to(controller: :users, action: :index)
       end
     end
   end
