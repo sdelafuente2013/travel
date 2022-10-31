ce74a050322d9a120f927e7901ef0d34f03852f7
delafuente <delafuente@tirant.com>
2022-10-31 17:17:44 +0100

    add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'guides/info'
-  get 'guides/form'
-  # get '/', to: 'users#index', as: 'user_root'
-  post 'guides/form', to: 'guides#save'
+  # devise_for :members
+
+  get 'form/create', to: 'forms#create_form'
+  post 'form/create', to: 'forms#send_mails'
+
+  get 'guides/dashboard', to: 'guides#dashboard'
+  get 'owners/dashboard', to: 'owners#dashboard'
+  get 'users/dashboard', to: 'users#dashboard'
+
+  devise_for :members, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'signup' }
 end
