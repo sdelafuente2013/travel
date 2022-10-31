a63e7765193a18c36d15edee218311b8ca4febc0
delafuente <delafuente@tirant.com>
2022-10-31 17:06:39 +0100

    fix: fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   get 'guides/info'
   get 'guides/form'
-  get '/', to: 'users#index', as: 'user_root'
+  # get '/', to: 'users#index', as: 'user_root'
   post 'guides/form', to: 'guides#save'
 end
