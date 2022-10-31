10f22fa8203c889c7da93f645b602adfbd27801c
delafuente <delafuente@tirant.com>
2022-10-31 17:13:30 +0100

    add: controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    return owners_dashboard_path if current_member.email == 'owner@example.com'
+    return users_dashboard_path if current_member.email == 'user@example.com'
+    return guides_dashboard_path if current_member.email == 'guide@example.com'
+  end
 end
