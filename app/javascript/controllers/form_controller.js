7dec259064c72e2e786ed53e728ff9fdb921b9c8
delafuente <delafuente@tirant.com>
2022-11-02 14:47:29 +0100

    fix: change validations with stimulus and refactor

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,42 +1,60 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="saludar"
 export default class extends Controller {
   name() {
-    if (this.element.value.length > 3){
+    try {
+      document.querySelector('.invalid-feedback').remove()
+    } catch (err) {
+    }
+
+    if (this.element.value.length > 3) {
       this.element.classList.remove('is-invalid')
       this.element.classList.add('is-valid')
-    }else{
+    } else {
       this.element.classList.remove('is-valid')
       this.element.classList.add('is-invalid')
     }
   }
 
   email() {
-    if (this.element.value.includes('@')){
+    try {
+      document.querySelector('.invalid-feedback').remove()
+    } catch (err) {
+    }
+
+    if (this.element.value.includes('@')) {
       this.element.classList.remove('is-invalid')
       this.element.classList.add('is-valid')
-    }else{
+    } else {
       this.element.classList.remove('is-valid')
       this.element.classList.add('is-invalid')
     }
   }
 
   password() {
-    if (this.element.value.length > 6){
+    try {
+      document.querySelector('.invalid-feedback').remove()
+    } catch (err) {
+    }
+    if (this.element.value.length > 6) {
       this.element.classList.remove('is-invalid')
       this.element.classList.add('is-valid')
-    }else{
+    } else {
       this.element.classList.remove('is-valid')
       this.element.classList.add('is-invalid')
     }
   }
 
   password_confirmation() {
-    if (this.element.value.length > 6 && this.element.value == document.querySelector('#contact_password').value){
+    try {
+      document.querySelector('.invalid-feedback').remove()
+    } catch (err) {
+    }
+    if (this.element.value.length > 6 && this.element.value == document.querySelector('#contact_password').value) {
       this.element.classList.remove('is-invalid')
       this.element.classList.add('is-valid')
-    }else{
+    } else {
       this.element.classList.remove('is-valid')
       this.element.classList.add('is-invalid')
     }
